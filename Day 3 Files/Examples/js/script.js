e10f635d6cd36bad99c3bf71a8f41b6155e630c2
CoryWilson <ctwilson08@gmail.com>
2014-05-09 15:33:14 -0400

    Area of Triangle
    Figured out the code for that.

--- Day 3 Files/Examples/js/script.js
+++ Day 3 Files/Examples/js/script.js
@@ -13,3 +13,16 @@ var birthYear = 1990;
 // Create a variable to hold the age
 var age = 2014 - birthYear;
 console.log(age);
+
+// Math
+// + - / *
+
+
+// Find the area of a Triangle
+// base * height * 1/2
+
+var base = 8;
+var height = 5;
+
+var areaTriangle = base*height*.5;
+console.log(areaTriangle);
