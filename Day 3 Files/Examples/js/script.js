f4da2a4f3b93c1ec1064ee39b874747a9effec42
CoryWilson <ctwilson08@gmail.com>
2014-05-09 16:30:19 -0400

    Order of Operations
    PEMDAS - look it up

--- Day 3 Files/Examples/js/script.js
+++ Day 3 Files/Examples/js/script.js
@@ -94,3 +94,28 @@ var maybe = "true";
 
 var no = false;
 
+// Order of operations
+// PEMDAS
+// Please Excuse My Dear Aunt Sally
+// Parenthesis Exponents Multiplication Division Addition Subtraction
+
+// Find the average of 4 Quiz Grades
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 70;
+var quiz4 = 80;
+
+// Find average, add all together and divide by total number of quizzes
+
+var average = (quiz1 + quiz2+ quiz3 + quiz4)/4;
+console.log(average);
+
+// DO NOT overuse parenthesis
+
+// Find the perimeter of a rectangle
+// Length * 2 + Width * 2
+var length = 7;
+var width = 6;
+var perimeter = (length*2) + (width*2);
+console.log(perimeter);
+
