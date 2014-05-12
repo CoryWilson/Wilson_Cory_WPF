46fd87e48cb80831257534fbb9d112948b52211c
CoryWilson <ctwilson08@gmail.com>
2014-05-12 18:17:25 -0400

    Personal Example First Idea
    Came up with first concept for a question. Wrote out givens

--- Day 4 Files/Day4LabExpressions/Expressions_Personal/js/script.js
+++ Day 4 Files/Day4LabExpressions/Expressions_Personal/js/script.js
@@ -8,8 +8,9 @@ Day 4 Expressions Assignment - Personal
 
 //Personal Example
 
-//Most efficient travel route
-/*Givens:
-   miles to and from school: a, b
-   time of travel
-*/
+//How many gallons of gas will be needed in order to successfully commute to and from school in a month's time?
+
+//Givens:
+//Average MPG of Car
+//Mileage of Commute
+//Amount of times travelled
