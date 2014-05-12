c97d19978bc1c5c9a6287d411d4b96b79c30361c
CoryWilson <ctwilson08@gmail.com>
2014-05-12 18:09:50 -0400

    Set up HTML and JS Files for Personal Example
    Outlined basic layout

---
+++ Day 4 Files/Day4LabExpressions/Expressions_Personal/js/script.js
@@ -0,0 +1,15 @@
+/*
+Cory Wilson
+WPF Section 1
+05/12/14
+Day 4 Expressions Assignment - Personal
+*/
+
+
+//Personal Example
+
+//Most efficient travel route
+/*Givens:
+   miles to and from school: a, b
+   time of travel
+*/
