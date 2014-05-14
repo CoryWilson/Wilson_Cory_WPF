c3d33ff9b295fc732e69c42440a62473ef89e76c
CoryWilson <ctwilson08@gmail.com>
2014-05-14 16:45:46 -0400

    Ternary Variable
    Set variable value using a ternary.

--- Day 5 Files/js/day5_ternary.js
+++ Day 5 Files/js/day5_ternary.js
@@ -20,3 +20,33 @@ if(gpa > 2.0){
 else {
   console.log("Tough shit you dummy.");
 }
+
+//(condition)? true code : false code;
+
+(gpa > 2.0) ? console.log("Congrats! You're gonna graduate!") : console.log("Tough shit you dummy");
+
+//Pick a book based on a kids age
+//Under 10 - Green Eggs and Ham - 10 & over gets Time Machine
+
+var age = 6;
+
+(age < 10) ? console.log("Green Eggs and Ham") : console.log("Time Machine");
+
+(age >= 10) ? console.log("Time Machine") : console.log("Green Eggs and Ham");
+
+//Save the book name to a variable
+
+var book;
+
+book = (age < 10) ? "Green Eggs and Ham" : "Time Machine";
+
+console.log("Your kid gets the book " + book + ".");
+
+if (age < 10){
+  book = "Green Eggs and Ham";
+}
+else{
+  book = "Time Machine";
+}
+
+console.log("Your kid gets the book " + book + ", in the form of an IF statement.");
