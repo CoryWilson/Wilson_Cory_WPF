437648911e719835058cb294c118729151fdc40e
CoryWilson <ctwilson08@gmail.com>
2014-05-14 13:58:27 -0400

    Basic if Statement & CSS File
    Learned about if statements.
    Added CSS File to the project.

--- Day 5 Files/js/day5_conditionals.js
+++ Day 5 Files/js/day5_conditionals.js
@@ -4,3 +4,22 @@ Cory Wilson
 WPF Section 1
 Day 5 Examples
 */
+
+//Basic Conditional - If Statement
+
+//If a child is tall enough, print to the console that he can ride a roller coaster
+
+var tallEnough = true;
+
+/*
+if(thing to evaluate){
+  Code to run, if it is true!
+}
+*/
+
+if(tallEnough) {
+  //Code will run if the kid is tall enough
+  console.log("You can ride the roller coaster!");
+}
+
+console.log("What comes after?");
