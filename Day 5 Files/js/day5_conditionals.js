f94d5158115033531ca764db446d9fcee8651d9c
CoryWilson <ctwilson08@gmail.com>
2014-05-14 15:01:43 -0400

    Else if Statements
    Elfish wait what?
    Check multiple conditions AND only run ONE block of code!

--- Day 5 Files/js/day5_conditionals.js
+++ Day 5 Files/js/day5_conditionals.js
@@ -32,6 +32,7 @@ else {
 
 //If a child is not over 48" tall, then he can't ride
 
+/*
 var kidHeight = 46;
 
 var minHeight = 48;
@@ -42,9 +43,34 @@ if(kidHeight + sneakerLift >= minHeight){
   //Code will run if the kid is tall enough
   console.log("Yeah, hop on dawg.");
 }
-  //Code will run if the kid is NOT tall enough
 else{
+  //Code will run if the kid is NOT tall enough
   console.log("You're not tall enough brah...");
 }
+*/
+
+//If a child is 48" or taller he can ride the coaster
+//If the child is riding with a parent, then he an be 45" tall. Anything smaller than that, No riding
+
+var kidHeight = 46;
+
+var minHeight = 48;
+
+var withParentHeight = 45;
+
+if(kidHeight >= minHeight){
+  //Code will run if the kid is tall enough
+  console.log("Yeah, hop on dawg.");
+}
+else if(kidHeight >= withParentHeight){
+  //Code will run if the kid is taller than 45" and   shorter than 48"
+  console.log("You can ride, but only wit a parent doe.");
+}
+else{
+  //Code will run if the kid is NOT tall enough
+  console.log("You're not tall enough brah...");
+}
+
+
 
 console.log("What comes after?");
