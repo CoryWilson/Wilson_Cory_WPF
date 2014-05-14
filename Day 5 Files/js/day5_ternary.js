daa2155143779a5ed34c1888096f42d29774fcf6
CoryWilson <ctwilson08@gmail.com>
2014-05-14 16:30:05 -0400

    Ternary Basic If Statement
    Starting file for Ternary

---
+++ Day 5 Files/js/day5_ternary.js
@@ -0,0 +1,22 @@
+/*
+Cory Wilson
+5/14/14
+WPF Section 1
+Day 5 Examples
+*/
+
+//Simplified Code for If Else
+//(condition to be met)? True Code : Else Code
+//(a>=b)? console.log("True") : console.log("False");
+
+//Check our GPA and see if we can graduate
+//Higher 2.0 GPA to graduate
+
+var gpa = 3.7;
+
+if(gpa > 2.0){
+  console.log("Congrats! You're gonna graduate!");
+}
+else {
+  console.log("Tough shit you dummy.");
+}
