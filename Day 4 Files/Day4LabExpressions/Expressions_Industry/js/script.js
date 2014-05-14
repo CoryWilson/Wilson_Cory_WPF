323db60191dbe569e52e732cf191aba6e4b74163
CoryWilson <ctwilson08@gmail.com>
2014-05-14 13:38:40 -0400

    Created Day 5 Files Folder
    Also created accompanying html and js files.

--- Day 4 Files/Day4LabExpressions/Expressions_Industry/js/script.js
+++ Day 4 Files/Day4LabExpressions/Expressions_Industry/js/script.js
@@ -4,3 +4,7 @@ WPF Section 1
 05/12/14
 Day 4 Expressions Assignment - Industry
 */
+
+//Question What can I write here???
+
+//
---
+++ Day 5 Files/js/day5_conditionals.js
@@ -0,0 +1,6 @@
+/*
+Cory Wilson
+5/14/14
+WPF Section 1
+Day 5 Examples
+*/
