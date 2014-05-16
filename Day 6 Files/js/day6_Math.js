0a3c982394494416e765d9d79aca3a5ffcde6814
CoryWilson <ctwilson08@gmail.com>
2014-05-16 15:23:40 -0400

    Math Class is in Session!
    Learning about rounding, floor, and ceiling.

---
+++ Day 6 Files/js/day6_Math.js
@@ -0,0 +1,35 @@
+/***************************************
+Cory Wilson
+WPF Section 1
+05/16/14
+Day 6 Examples
+Math Class
+***************************************/
+
+//Math.ceil(num); Rounds up to the next integer
+//Math.floor(num); Rounds down to the next integer
+//Math.random();
+
+//Always capitalize the M in Math.
+
+
+//Math.round()
+//round
+//Normal Rounding .5 goes up, less goes down.
+var num1 = 9.544;
+console.log(num1);
+console.log(Math.round(num1));
+
+//Math.floor()
+//floor
+//Chops off all decimals and always rounds down.
+var num2 = 6.99;
+console.log(num2);
+console.log(Math.floor(num2));
+
+//Math.ceil()
+//ceiling
+//Always rounds up!
+var num3 = 4.23;
+console.log(num3);
+console.log(Math.ceil(num3));
