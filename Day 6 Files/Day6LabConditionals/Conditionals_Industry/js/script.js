b0c71e931f6f3d361c06bd2a224df550ae68ed01
CoryWilson <ctwilson08@gmail.com>
2014-05-16 17:10:01 -0400

    Solved the logic problem for this question
    used logical operators and else if statements to execute the problem.

--- Day 6 Files/Day6LabConditionals/Conditionals_Industry/js/script.js
+++ Day 6 Files/Day6LabConditionals/Conditionals_Industry/js/script.js
@@ -13,14 +13,25 @@ Day 6 Conditionals Assignment - Industry
 
 //Givens:
 //Does it support basic text editing?
-var textEdit = Boolean(prompt());
+var textEdit = prompt("Does the text editor allow for basic text editing? True or False?");
 //Does it cost money?
-var cost = Boolean(prompt());
+var cost = prompt("Does the text editor cost money? True or False?");
 //Does it autocomplete text?
-var autoComplete = Boolean(prompt("Does the text editor allow for autocompletion of text?"));
+//var autoComplete = prompt("Does the text editor allow for autocompletion of text? True or False?");
 //Is it customizable?
-var custom = Boolean(prompt("Does the text editor allow for customization of the program?"));
+//var custom = prompt("Does the text editor allow for customization of the program? True or False?");
 
+if(textEdit === "true" && cost === "true"){
+  console.log("Good Shit");
+}
+else if(textEdit === "true" || cost === "true"){
+  console.log("This is a decent text editor. It may be missing some necessary features.");
+}
+else{
+  console.log("no bueno");
+}
+
+/*
 //If the program has both autoComplete and custom it is a success
 if(autoComplete  && custom ){
   console.log("This is a great text editor, it has all the necessary features.");
@@ -33,3 +44,4 @@ else if(autoComplete || custom ){
 else{
   console.log("Don't waste your time. This is a poor text editor.");
 }
+*/
