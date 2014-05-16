b2992f46be8f67152b6cc45796ee3c30a538de05
CoryWilson <ctwilson08@gmail.com>
2014-05-16 14:59:31 -0400

    Working Nesting If Statement
    Everything is working properly

--- Day 6 Files/js/day6_Nestingifs.js
+++ Day 6 Files/js/day6_Nestingifs.js
@@ -16,21 +16,26 @@ Nesting If Statements
 
 
 //Create variable for outside temp.
-var temp = 90; //degrees
+var temp = Number(prompt("What's the temperature outside?")); //degrees
 
 //Water temp
-var waterTemp = 78; //degrees
+//Move waterTemp inside the if statement?
+//var waterTemp = Number(prompt("What's the water temperature?")); //degrees
 
 if (temp >= 85){
   console.log("Let's go to the beach!");
+
+  //Only ask if we're going to the beach!
+  var waterTemp = Number(prompt("What's the water temperature?")); //degrees
+
+  if(waterTemp >= 75){
+    console.log("I'm going in the water!");
+  }
+  else{
+    console.log("Let's get a tan.");
+  }
+
 }
 else{
   console.log("Let's go to the movies!");
-}
-
-if(waterTemp >= 75){
-  console.log("I'm going in the water!");
-}
-else{
-  console.log("Let's get a tan.");
 }
