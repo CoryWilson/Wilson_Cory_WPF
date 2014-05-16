afb02a41bd71c6b6bf4efca739e6e5eec5c6a94d
CoryWilson <ctwilson08@gmail.com>
2014-05-16 17:33:15 -0400

    Personal Question
    came up with question and started to run through the logic.

--- Day 6 Files/Day6LabConditionals/Conditionals_Personal/js/script.js
+++ Day 6 Files/Day6LabConditionals/Conditionals_Personal/js/script.js
@@ -11,6 +11,24 @@ Day 6 Conditionals Assignment - Personal
 
 //utilize a ternary in here somewhere
 
+
+//Did you break par in your round of golf?
+
+//Givens:
+//Par for the golf course
+//Score shot for your round
+
+
 //need two givens at least
-var thing1 = prompt();
-var thing2 = prompt();
+var coursePar = Number(prompt("What is the par for the golf course?"));
+var scoreShot = Number(prompt("What did you shoot for your round today?"));
+
+if(scoreShot < coursePar){
+  console.log("Congrats! You have broken par! Excellent round.");
+}
+else if(scoreShot = coursePar){
+  console.log("Almost, you shot par. Still an great round.");
+}
+else{
+  console.log("Sorry, you shot over par. Go practice some more.");
+}
