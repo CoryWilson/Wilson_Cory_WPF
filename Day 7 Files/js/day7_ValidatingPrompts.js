3cd074bbfad43e45b82e281ecd9bdf08d1cd19d1
CoryWilson <ctwilson08@gmail.com>
2014-05-19 17:59:50 -0400

    Validating Prompts

--- Day 7 Files/js/day7_ValidatingPrompts.js
+++ Day 7 Files/js/day7_ValidatingPrompts.js
@@ -9,8 +9,29 @@ Day 7 Validating Prompts
 //Create a random number in between those two numbers
 alert("Let's find a random number between two numbers!");
 var min = prompt("Please type in a minimum number:");
+
+//Validate with a Loop
+//Use a while loop
+
+while(isNaN(min) || min===""){
+  //Its a text string, prompt again for a number
+  min = Number(prompt("Please type in a Minimum NUMBER!"));
+}
+
 var max = Number(prompt("Please type in a maximum number:"));
 
+while(isNaN(max) || min===""){
+  max = Number(prompt("Please type in a Maximum NUMBER!"));
+}
+
+//Prompt for first name
+var name = prompt("What is your first name?");
+
+while(name===""){
+  name = prompt("Pleas type in something for your name!");
+}
+
+/*********************************************************
 //Validate our user's responses
 //Empty String
 if(min===""){
@@ -25,12 +46,13 @@ if(min===""){
 //Text string returns true
 console.log(isNaN(max));
 
+
 if(isNaN(max)){
   //Will run if its true
   //Max is a string not a number
   max = Number(prompt("Please type in a NUMBER!!"));
 }
-
+***********************************************************/
 
 //Generate a random Integer
 //Math.random()*(max-min) + min;
