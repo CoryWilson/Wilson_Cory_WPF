de9de53ee1acbf31830e52397b66382305a70c9a
CoryWilson <ctwilson08@gmail.com>
2014-05-19 14:16:42 -0400

    Do While Example
    Do While loop will run at least once and then test the conditional.

--- Day 7 Files/js/day7_WhileLoops.js
+++ Day 7 Files/js/day7_WhileLoops.js
@@ -17,8 +17,26 @@ while(counter < 200){ //condition - loops until false
   console.log("Something's just never end.");
   //Change the value of the counter variable
   //OR it will run FOREVER!! No Bueno!
-  console.log("The counter is " + counter);
+  console.log("The counter is " + counter + ".");
   counter++;
 }
 
-console.log("Ending counter " + counter);
+console.log("Ending counter " + counter + ".");
+
+//Do While Loop
+//This loop will run the code once to start, before checking if the condition is true.
+//THEN it will repeat as long as the condition stays true.
+
+//Initialize the variable
+var i = 20;
+
+//Setup the do while loop
+do{
+  console.log("The number of i is " + i + ".");
+
+  //Change the value of the variable to avoid and infinite loop
+  i++;
+}
+while(i<10); //Condition to test
+
+console.log("The number of i following the do while loop is " + i + ".");
