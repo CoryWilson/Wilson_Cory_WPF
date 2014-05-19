256c000245089fcd0ce8224d4cba656d23914a9c
CoryWilson <ctwilson08@gmail.com>
2014-05-19 18:31:37 -0400

    Reorganized Assignment Files
    Reorganized Assignment Files

---
+++ Day 7 Files/Loops_worksheet/while/js/while.js
@@ -0,0 +1,32 @@
+/***************************************
+Cory Wilson
+WPF Section 1
+05/19/14
+Day 7 While Loops
+***************************************/
+
+//Figure out how many attempts it will take to accomplish something...
+
+/*
+var numZombies = 1; //How many zombies do we have?
+var numBites = 4; //# of bites per zombie per day
+var days = 9; //# of days
+*/
+
+/*
+var numDays = 0;
+
+while(numZombies < 7000000000){
+
+  numDays++;
+
+  var newZombies = numZombies * numBites;
+
+  //Add the new zombies to the total number of zombies
+  numZombies += newZombies; //numZombies  = numZombies + newZombies
+  console.log("There are "+numZombies+" zombies at the end of day #"+numDays+".");
+
+}
+
+console.log("It will take "+numDays+" for zombies to take over the world.");
+*/
