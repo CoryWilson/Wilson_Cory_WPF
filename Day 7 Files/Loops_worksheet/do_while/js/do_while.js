c9bb6d2e134ac0632244647a7ea8e8a078208527
CoryWilson <ctwilson08@gmail.com>
2014-05-19 19:19:32 -0400

    While and Do While
    Worked on the two pages. Trying to understand the difference between
    the two types of loops.

--- Day 7 Files/Loops_worksheet/do_while/js/do_while.js
+++ Day 7 Files/Loops_worksheet/do_while/js/do_while.js
@@ -4,3 +4,15 @@ WPF Section 1
 05/19/14
 Day 7 For Loops
 ***************************************/
+
+//Initialized Variables
+var var1=Number(prompt("Variable 1 "));
+var var2=Number(prompt("Variable 2 "));
+
+//Setup the do while loop
+do{
+  console.log("Stuff goes in here "+var1+".");
+  //Changing the value of the variable to avoid an infinite loop
+  var1++;
+}
+while(var1<var2); //Condition to test
--- Day 7 Files/Loops_worksheet/while/js/while.js
+++ Day 7 Files/Loops_worksheet/while/js/while.js
@@ -7,26 +7,20 @@ Day 7 While Loops
 
 //Figure out how many attempts it will take to accomplish something...
 
-/*
-var numZombies = 1; //How many zombies do we have?
-var numBites = 4; //# of bites per zombie per day
-var days = 9; //# of days
-*/
+//Initialize a Counter
+alert("We're going to count up to any number you want to.");
+var counter = 0;
+var num = Number(prompt("Put a number you would like to count up to in here..."));
 
-/*
-var numDays = 0;
-
-while(numZombies < 7000000000){
-
-  numDays++;
-
-  var newZombies = numZombies * numBites;
-
-  //Add the new zombies to the total number of zombies
-  numZombies += newZombies; //numZombies  = numZombies + newZombies
-  console.log("There are "+numZombies+" zombies at the end of day #"+numDays+".");
-
+//Validate prompt
+while(isNaN(num) || num===""){
+  //Its a text string, prompt again for a number
+  num = Number(prompt("Please type in a NUMBER!"));
 }
 
-console.log("It will take "+numDays+" for zombies to take over the world.");
-*/
+//Setup a while loop
+while(counter < num){
+  console.log(counter);
+  counter++;
+}
+console.log(counter);
