5b08b50263bdf329a2683e0b5288b014fd4b5504
CoryWilson <ctwilson08@gmail.com>
2014-05-19 20:33:31 -0400

    Complete do while
    Finished my do while example.

--- Day 7 Files/Loops_worksheet/do_while/js/do_while.js
+++ Day 7 Files/Loops_worksheet/do_while/js/do_while.js
@@ -6,22 +6,31 @@ Day 7 Do While Loops
 ***************************************/
 
 //Initialized Variables
-var var1=Number(prompt("Insert a number in here:"));
+//Make a program that will run until number 1 matches number 2
+alert("Let's check to see if Number 1 remains less than Number 2");
+//Ask for the value of number 1
+var num1=Number(prompt("Insert a number in here:"));
 //Validate Prompt
-while(isNaN(var1) || var1===""){
+while(isNaN(num1) || num1===""){
   //Its a text string, prompt again for a number
-  var1 = Number(prompt("Please type in a NUMBER!"));
+  num1 = Number(prompt("Please type in a NUMBER!"));
 }
-var var2=Number(prompt("Insert another number in here:"));
+//Ask for the value of number 2
+var num2=Number(prompt("Insert another number in here:"));
 //Validate Prompt
-while(isNaN(var2) || var2===""){
+while(isNaN(num2) || num2===""){
   //Its a text string, prompt again for a number
-  var2 = Number(prompt("Please type in a NUMBER!"));
+  num2 = Number(prompt("Please type in a NUMBER!"));
 }
 //Setup the do while loop
 do{
-  console.log("Stuff goes in here "+var1+".");
+  if(num1>num2){
+    console.log(num1+" is larger than "+num2+" don't bother running.");
+  }
+  else{
+  console.log(num1+" is still less than "+num2+".");
+  }
   //Changing the value of the variable to avoid an infinite loop
-  var1++;
+  num1++;
 }
-while(var1<var2); //Condition to test
+while(num1<num2); //Condition to test
