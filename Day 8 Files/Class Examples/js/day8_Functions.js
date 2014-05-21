454f9fd2af429560ff3a1a90662990bf47973c6e
CoryWilson <ctwilson08@gmail.com>
2014-05-21 15:13:31 -0400

    Basic Function
    Create basic function and call it.

--- Day 8 Files/Class Examples/js/day8_Functions.js
+++ Day 8 Files/Class Examples/js/day8_Functions.js
@@ -4,3 +4,19 @@ WPF Section 1
 05/21/14
 Day 8 Functions
 ***************************************/
+
+//Basic Function
+//function functionName(){}
+
+//Create a function that console.log hello
+
+function printHello(){
+  console.log("Hello!");
+}
+
+//Function Call our printHello function
+//"Go Button"
+
+//functionName()
+
+printHello(); //Function Call or Invocation
