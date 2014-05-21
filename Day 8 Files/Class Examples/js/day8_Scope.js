17d6de3a21e098a0d13a4e0c995f26df4544e949
CoryWilson <ctwilson08@gmail.com>
2014-05-21 15:54:20 -0400

    Variable Scope
    variables created in a function STAY in a function

--- Day 8 Files/Class Examples/js/day8_Scope.js
+++ Day 8 Files/Class Examples/js/day8_Scope.js
@@ -12,15 +12,24 @@ Day 8 Scope
 //This is going to be the impossible in LARGE files
 
 //Create a variable for Width in our MAIN code
-var width = 5;
+var width = 5; //Scoped outside of the function - "Main Code"
 
 //Create a function that calculates the perimeter of a rectangle
 function calcPeri(){
-  width = 28;
-  console.log(width);
+  //can create new variables inside of functions, but it cannot leave that function
+  //only valid within that box!!!
+  var width = 10; //Scoped to the function calcPeri
+  //width = 10; would be scoped outside of the function
+  console.log("Inside of function "+width);
+  var height = 20;
+  var perimeter = width*2 + height*2;
+  console.log("Inside of function, the perimeter is "+perimeter);
+  //Variables created in a function, can NOT be accessed outside of a function
 }
 
 console.log("Before Call "+width);
 calcPeri();
 
 console.log("After Call "+width);
+
+console.log("After call the perimeter is "+perimeter);
