1c0b21d16e9553116fd6d54a067abb1fb9f1a452
CoryWilson <ctwilson08@gmail.com>
2014-05-21 15:21:58 -0400

    calcArea()
    created and called calcArea() function.

--- Day 8 Files/Class Examples/js/day8_Functions.js
+++ Day 8 Files/Class Examples/js/day8_Functions.js
@@ -9,14 +9,33 @@ Day 8 Functions
 //function functionName(){}
 
 //Create a function that console.log hello
-
 function printHello(){
   console.log("Hello!");
+}
+
+//Create a function that console.log "prints out more"
+function printMore(){
+  console.log("Prints out more text.");
 }
 
 //Function Call our printHello function
 //"Go Button"
 
 //functionName()
-
 printHello(); //Function Call or Invocation
+printMore();
+printHello();
+printMore();
+
+//Function Call calcArea
+calcArea();
+
+//Create a function that calculates the area of a rectangle
+function calcArea(){
+  //Create variables for width, height, and area
+  var width = 20;
+  var height = 30;
+  var area = width * height;
+  //Print out the area
+  console.log("Area of the rectangle is "+area+".");
+}
