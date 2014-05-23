1183eeb21d34007dacdc70fbb4cc9bb3874c452b
CoryWilson <ctwilson08@gmail.com>
2014-05-23 14:07:21 -0400

    Remember to CAST
    Added in user prompts

--- Day 9 Files/Class Examples/js/day9_RandomNumbers.js
+++ Day 9 Files/Class Examples/js/day9_RandomNumbers.js
@@ -20,12 +20,13 @@ function randomizer(mn,mx){ //Use Parameters
 }
 
 //Ask user for min & max values
+//Cast user prompts to Numbers
 var minUser = Number(prompt("Please insert a min value"));
 var maxUser = Number(prompt("Please insert a max value"));
-//Function call this randomizer
 
+//Function call this randomizer
 //NEED a variable to catch the returned value
-var result = randomizer(20,100); //NEED arguments
+var result = randomizer(minUser,maxUser); //NEED arguments
 
 //Console log it out
 console.log(result);
