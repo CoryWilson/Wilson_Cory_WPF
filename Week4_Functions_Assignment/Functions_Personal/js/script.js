da856c3451daf94b2f7f53ca6d3045eac13c30e3
CoryWilson <ctwilson08@gmail.com>
2014-05-23 18:40:21 -0400

    Comment code
    Make sure the code is appropriately commented.

--- Week4_Functions_Assignment/Functions_Personal/js/script.js
+++ Week4_Functions_Assignment/Functions_Personal/js/script.js
@@ -10,32 +10,42 @@ Functions Assignment - Personal
 //Budget, Ticket Cost
 //If price difference is > $50 then yes
 
-alert("Let's see if you can attend a concert this week!");
+//Let the user know that we are going to check to see if they can attend a concert this week
+alert("Let's see if you can attend a concert this week! If you have less than $50 left over than you cannot attend the concert.");
 
+//First ask the user for their weekly budget
 var weeklyBudget = Number(prompt("How large is your budget this week?"));
+//Validate the prompt
 while(isNaN(weeklyBudget) || weeklyBudget===""){
+  //Ask the user to insert a number if they input something else
   weeklyBudget = Number(prompt("Please insert a number value for your weekly budget."));
 }
 
+//Then ask the user how much the ticket costs
 var ticketCost = Number(prompt("How large is your budget this week?"));
+//Validate the prompt
 while(isNaN(ticketCost) || ticketCost===""){
+  //Ask the user to insert a number if they input something else
   ticketCost = Number(prompt("Please insert a number value for the cost of the ticket."));
 }
 
+//function that finds out the cost difference between your budget and the cost of the ticket.
 function concert(b,t){
 
-  var concertYes = b-t;
-
-  return concertYes;
+  var concertDiff = b-t;
+  //return the difference value
+  return concertDiff;
 
 }
 
+//store the returned value in the result variable
 var result = concert(weeklyBudget,ticketCost);
+//check that value with a console log
 console.log(result);
 
+//finally compare the returned value of the concert function and make sure that you have more than $50 left over between the budget and concert
 if(result > 50){
   console.log("Congratulations your budget is $"+weeklyBudget+" and the ticket to the concert costs $"+ticketCost+". That means you can go to the concert this week!");
-}
-else{
-  console.log("Try again next week. :(");
+} else{
+  console.log("You don't have enough money. Try again next week. :(");
 }
