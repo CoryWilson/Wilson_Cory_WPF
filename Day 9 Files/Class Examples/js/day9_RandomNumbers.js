5cccbe0e1370c41854b5a40dedb302d61a36c1c7
CoryWilson <ctwilson08@gmail.com>
2014-05-23 14:01:48 -0400

    Function Assignment Files Created / RandomNumbers
    …  Worked through random number function

--- Day 9 Files/Class Examples/js/day9_RandomNumbers.js
+++ Day 9 Files/Class Examples/js/day9_RandomNumbers.js
@@ -1,6 +1,28 @@
-/***************************************
+/*******************
 Cory Wilson
 WPF Section 1
 05/23/14
 Day 9 Random Numbers
-***************************************/
+*******************/
+
+//Create a function that will give a random integer
+
+function randomizer(mn,mx){ //Use Parameters
+
+  //Generate a random integer and RETURN it back
+
+  //Create a variable to hold our random integer
+  var randomNum = Math.round(Math.random()*(mx-mn)+mn);
+
+  //Return the random integer
+  return randomNum;
+
+}
+
+//Function call this randomizer
+
+//NEED a variable to catch the returned value
+var result = randomizer(20,100); //NEED arguments
+
+//Console log it out
+console.log(result);
---
+++ Week4_Functions_Assignment/Functions_Personal/js/script.js
@@ -0,0 +1,6 @@
+/******************************
+Cory Wilson
+WPF Section 1
+05/23/14
+Functions Assignment - Personal
+******************************/
