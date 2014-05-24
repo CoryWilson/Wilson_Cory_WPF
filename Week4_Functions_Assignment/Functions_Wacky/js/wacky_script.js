41ea36fcc330134640793f593c75e74f4a91d03a
CoryWilson <ctwilson08@gmail.com>
2014-05-23 20:19:16 -0400

    Lots of ideas.
    Not sure if any of them are right! :/

--- Week4_Functions_Assignment/Functions_Wacky/js/wacky_script.js
+++ Week4_Functions_Assignment/Functions_Wacky/js/wacky_script.js
@@ -17,10 +17,13 @@ Functions Assignment - Wacky
 
 //user inputs results
 //validate prompts
-var match1 = prompt("Did the team win, draw, or lose match one?");
-while(match1===""){
-  var match1 = prompt("Make sure to enter a result!");
+for(var i=0; i<3; i++){
+var match = prompt("Did the team win, draw, or lose match one?");
+while(match===""){
+  var match = prompt("Make sure to enter a result!");
 }
+
+/*
 var match2 = prompt("Did the team win, draw, or lose match two?");
 while(match2===""){
   var match1 = prompt("Make sure to enter a result!");
@@ -29,6 +32,33 @@ var match3 = prompt("Did the team win, draw, or lose match three?");
 while(match3===""){
   var match3 = prompt("Make sure to enter a result!");
 }
+*/
+
+//team enters win -> pull win function -> adds to total
+//team enters draw -> pull draw function -> adds to total
+//team enters loss -> pull loss function -> adds to total
+
+
+function result(match){
+
+  if(match==="win"){
+    var points = 3;
+  } else if(match==="draw"){
+    var points = 1;
+  } else{
+    var points = 0;
+  }
+  return points;
+}
+
+var matchResults = result(match);
+
+for(var i = 0; i < 3; i++){
+  matchResults += matchResults;
+  console.log(matchResults);
+}
+}
+/*
 //function that returns points for a loss
 var loss = function(){
    var points = 0;
@@ -44,7 +74,4 @@ var win = function(){
   var points = 3;
   return points;
 }
-
-for(var i = 0; i<3; i++){
-
-}
+*/
