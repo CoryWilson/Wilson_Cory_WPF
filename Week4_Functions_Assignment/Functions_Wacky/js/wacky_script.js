166f7231cecfc478144840f7174463922b13f059
CoryWilson <ctwilson08@gmail.com>
2014-05-27 13:07:51 -0400

    Added Functions for win, draw, loss
    made the program add team name and final points total to console log.
    now trying to figure out how to compare results.

--- Week4_Functions_Assignment/Functions_Wacky/js/wacky_script.js
+++ Week4_Functions_Assignment/Functions_Wacky/js/wacky_script.js
@@ -15,22 +15,23 @@ Functions Assignment - Wacky
 //function draw - 1 point
 //function win - 3 points
 
-//3 matches, so we need 3 prompts
-//validate each prompt
-/*
-var match1 = prompt("Did the team win, draw, or lose match one?");
-while(match1===""){
-  var match1 = prompt("Make sure to enter a result!");
+function win(){
+  var result = 3;
+  return result;
 }
-var match2 = prompt("Did the team win, draw, or lose match two?");
-while(match2===""){
-  var match2 = prompt("Make sure to enter a result!");
+
+function draw(){
+  var result = 1;
+  return result;
 }
-var match3 = prompt("Did the team win, draw, or lose match three?");
-while(match3===""){
-  var match3 = prompt("Make sure to enter a result!");
+
+function loss(){
+  var result = 0;
+  return result;
 }
-*/
+
+alert("Let's figure out how the group stage of the world cup ended up.");
+
 //team enters win -> pull win function -> adds to total
 //team enters draw -> pull draw function -> adds to total
 //team enters loss -> pull loss function -> adds to total
@@ -46,9 +47,9 @@ var team3Name = prompt("What country is the third team?");
 while(team3Name===""){
   var team3Name = prompt("Make sure to enter a team name!");
 }
-var team2Name = prompt("What country is the fourth team?");
-while(team2Name===""){
-  var team2Name = prompt("Make sure to enter a team name!");
+var team4Name = prompt("What country is the fourth team?");
+while(team4Name===""){
+  var team4Name = prompt("Make sure to enter a team name!");
 }
 
 
@@ -64,17 +65,17 @@ while(team1===""){
 }
 
 if(team1 === "win"){
-  var points = 3;
+  var points = win();
 }  else if(team1 === "draw"){
-  var points = 1;
+  var points = draw();
 }  else{
-  var points = 0;
+  var points = loss();
 }
 console.log(points);
 teamOnePoints += points;
 
 }
-console.log(teamOnePoints);
+console.log(team1Name+" finished the group stage with a total of "+teamOnePoints+" points.");
 
 /********
 Team Two!
@@ -82,68 +83,66 @@ Team Two!
 var teamTwoPoints = 0;
 
 for(var i = 0; i < 3; i++){
-var team2 = prompt("Did the team win, draw, or lose the match?");
+var team2 = prompt("Did team "+team2Name+" win, draw, or lose the match?");
 while(team2===""){
   var team2 = prompt("Make sure to enter a result!");
 }
 
 if(team2 === "win"){
-  var points = 3;
+  var points = win();
 }  else if(team2 === "draw"){
-  var points = 1;
+  var points = draw();
 }  else{
-  var points = 0;
+  var points = loss();
 }
 console.log(points);
 teamTwoPoints += points;
 
 }
-console.log(teamTwoPoints);
-
+console.log(team2Name+" finished the group stage with a total of "+teamTwoPoints+" points.");
 /**********
 Team Three!
 **********/
 var teamThreePoints = 0;
 
 for(var i = 0; i < 3; i++){
-var team3 = prompt("Did the team win, draw, or lose the match?");
+var team3 = prompt("Did team "+team3Name+"  win, draw, or lose the match?");
 while(team3===""){
   var team3 = prompt("Make sure to enter a result!");
 }
 
 if(team3 === "win"){
-  var points = 3;
+  var points = win();
 }  else if(team3 === "draw"){
-  var points = 1;
+  var points = draw();
 }  else{
-  var points = 0;
+  var points = loss();
 }
 console.log(points);
 teamThreePoints += points;
 
 }
-console.log(teamThreePoints);
-
+console.log(team3Name+" finished the group stage with a total of "+teamThreePoints+" points.");
 /*********
 Team Four!
 *********/
 var teamFourPoints = 0;
 
 for(var i = 0; i < 3; i++){
-var team4 = prompt("Did the team win, draw, or lose the match?");
+var team4 = prompt("Did team "+team4Name+" win, draw, or lose the match?");
 while(team4===""){
   var team4 = prompt("Make sure to enter a result!");
 }
 
 if(team4 === "win"){
-  var points = 3;
+  var points = win();
 }  else if(team4 === "draw"){
-  var points = 1;
+  var points = draw();
 }  else{
-  var points = 0;
+  var points = loss();
 }
 console.log(points);
 teamFourPoints += points;
 
 }
-console.log(teamFourPoints);
+console.log(team4Name+" finished the group stage with a total of "+teamFourPoints+" points.");
