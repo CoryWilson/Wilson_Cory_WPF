5caae25f7c29c0ed66ce9ec71057f15c8f9c741c
CoryWilson <ctwilson08@gmail.com>
2014-05-28 19:39:51 -0400

    Back to Original

--- Day 3 Files/Day3LabExpressions/js/script.js
+++ Day 3 Files/Day3LabExpressions/js/script.js
@@ -1,199 +1,105 @@
-/*******************************
+/*
  Cory Wilson
- 05/28/14
- WPF Section 1
- Day 3 Lab Expressions - Revised
- ******************************/
+ 05/09/14
+ WPF 1405
+ Day 3 Lab Expressions
+ */
 
 
-/******************
+
+
+/*
 Slice of Pie Part 1
-******************/
+*/
 
 //Calculate the the slices each partygoer gets
 //How many slices per pizza
-var slices = Number(prompt("How many slices of pizza are there?"));
-//validate
-while(isNaN(slices) || slices===""){
-  slices = Number(prompt("Please insert a NUMBER for pizza slices!"));
-}
+var slices = 10;
 //How many people are at the party
-var people = Number(prompt("How many people are at the party?"));
-//validate
-while(isNaN(people) || people===""){
-  people = Number(prompt("Please insert a NUMBER for the amount of people at the party!"));
-}
+var people = 7;
 //How many pizzas they ordered
-var pizzas = Number(prompt("How many pizzas did they order?"));
-//validate
-while(isNaN(pizzas) || pizzas===""){
-  pizzas = Number(prompt("Please insert a NUMBER for pizzas ordered!"));
-}
-/*
+var pizzas = 5;
+
 //Slices per person = Multiply the number of slices and pizzas together to get the total number of slices. Then divide that total by the amount of people at the party
-var slicePerPerson = (slices * pizzas) / people;
+var peopleSlices = (slices * pizzas) / people;
 //Output Area
 console.log("Each person ate " + peopleSlices + " slices of pizza at the party.");
+
+
+/*
+Slice of Pie Part 2
 */
 
-//created anonymous function to calculate slices per person
-//parameters for slices, pizzas, people
-var peopleSlices = function(sl,pz,pe){
-  var slicesPerPerson = (sl*pz)/pe;
-  var sPP = slicesPerPerson.toFixed(2);
-  return sPP;
-}
-//insert prompts as arguments
-//viola!
-var result = peopleSlices(slices,pizzas,people);
-console.log("Each person ate "+result+" slices of pizza at the party.");
-
-
-/******************
-Slice of Pie Part 2
-******************/
+/*
+var slices = 10;
+var people = 10;
+var pizzas = 5;
+*/
 
 //Total number of slices
-//var totalSlices = slices * pizzas;
-//Create anonymous function (procedure) for totalSlices
-var totalSlices = function(s,p){
-  var total = s*p;
-  return total;
-}
-var tSResult = totalSlices(slices, pizzas);
+var totalSlices = slices * pizzas;
 
 //Calculate the number of slices Sparky eats
-//var sparkySlices = totalSlices % people ;
-var sparkySlices = function(t,p){
-  var total = t%p;
-  return total;
-}
-var sSlicesResult = sparkySlices(tSResult,people);
-
+var sparkySlices = totalSlices % people ;
 
 //Output Area
-console.log("Sparky got "+sSlicesResult+" slices of pizza.");
+console.log("Sparky got " + sparkySlices + " slices of pizza.");
 
 
-/********************
+
+
+/*
 Average shopping bill
-********************/
+*/
 
 //Calculate average shopping bill for the past five weeks
-alert("This program is going to average out your shopping bills. Just tell it how many weeks you want to average and have at it!");
-//Ask the user for the amount of weeks they want to average
-var amountWeeks = Number(prompt("How many weeks did you want to average your shopping bill for?"));
-while(isNaN(amountWeeks) || amountWeeks===""){
-  amountWeeks = Number(prompt("Please insert a NUMBER for weeks!"));
-}
 
-//create a blank array that will hold each individual weeks
-var avgArray = [];
-//loop through that array by the amount of weeks inputed earlier
-//asking the user what the shopping bill was each week
-for(var i=0; i<amountWeeks; i++){
-  var input = Number(prompt("What is your shopping bill this week?"));
-  //push the input result into the array adding new items
-  avgArray.push(input);
-}
+//Week 1 Bills
+var week1 = 50;
+//Week 2 Bills
+var week2 = 120;
+//Week 3 Bills
+var week3 = 110;
+//Week 4 Bills
+var week4 = 70;
+//Week 5 Bills
+var week5 = 190;
 
-//begin the totaling process
-//set a total variable equal to 0
-var total = 0;
-//loop through the lengh of the array
-for(var j=0; j < avgArray.length; j++){
-  //add each item in the array to the total variable
-  total += avgArray[j];
-}
+//Total spent on groceries
+var total = week1 + week2 + week3 + week4 + week5;
 
-//anonymous function that averages using the total and amount of weeks acquired earlier
-//parameter of t aka total and w for weeks
-var averager = function(t,w){
-  //avg = t/
-  var avg = t/w;
-  return avg;
-}
-//input the arguments for total and amount of weeks
-var results = averager(total,amountWeeks);
-//bring result to two decimal places
-var r = results.toFixed(2);
-//concatinate a string that includes weeks and final result
-console.log("Over the period of "+amountWeeks+ " weeks you spent a total of $"+total+" on shopping bills and an average amount of $"+r+".");
+//Average weekly grocery spending
+var average = total / 5;
+
+//Output Area
+console.log("You have spent a total of $" + total + " on groceries over 5 weeks. That is an average of $" + average + " per week.");
 
 
-/********
+
+/*
 Discounts
-********/
+*/
 
-//Calculate the discounted price for an item. With and without sales tax. New and Improved Version.
+//Calculate the discounted price for an item. With and without sales tax.
 
-//Ask the use for the name of the product they are looking to purchase
-var itemDescription = prompt("What product are you looking to buy?");
-//Validate
-while(itemDescription===""){
-  itemDescription = prompt("Please enter a product name!");
-}
-//Ask them what store they are going to buy the product from
-var store = prompt("What store are you going to be shopping at for "+itemDescription+"?");
-//Validate
-while(store===""){
-  store = prompt("Please enter a store name!");
-}
-//Ask them how much the item costs
-var originalPrice = Number(prompt("How much does the "+itemDescription+" cost?"));
-//Validate
-while(isNaN(originalPrice) || originalPrice===""){
-  originalPrice = prompt("Please enter a NUMBER for product price!");
-}
+//Original Price
+var originalPrice = 500;
+//Discount Percentage
+var discountPercentage = .05;
+//Item Description
+var itemDescription = "Xbox One";
+//Sales Tax Percentage
+var salesTaxPercentage = .07;
+//Discount Amount
+var discountAmount = originalPrice * discountPercentage;
+//Sales Tax Amount
+var salesTaxAmount = originalPrice * salesTaxPercentage;
 
-//Ask them what the store's discount percentage is...
-var discountPercentage = Number(prompt("What is the discount percentage at "+store+"?"));
-//Validate
-while(isNaN(discountPercentage) || discountPercentage===""){
-  discountPercentage = prompt("Please enter a NUMBER for "+store+"'s discount percentage!");
-}
-//turn that number into a percentage
-discPerc = discountPercentage/100;
-//ask them what the store's sales tax percentage is...
-var salesTaxPercentage = Number(prompt("What is the sales tax percentage at "+store+"?"));
-//Validate
-while(isNaN(salesTaxPercentage) || salesTaxPercentage===""){
-  salesTaxPercentage = prompt("Please enter a NUMBER for "+store+"'s sales tax percentage!");
-}
-//turn that number into a percentage
-salesPerc = salesTaxPercentage/100;
+//Price of item with tax
+var itemWithTax = originalPrice + salesTaxAmount - discountAmount;
 
-//anon function for discount amount on item
-var discountAmount = function(op,dp){
-  var disc = op * dp;
-  return disc;
-}
-//call the function
-var dAmount = discountAmount(originalPrice,discPerc);
+//Price of item without tax
+var itemWithoutTax = originalPrice - discountAmount;
 
-//anon function for sales tax amount on item
-var salesTaxAmount = function(op,sp){
-  var sales = op * sp;
-  return sales;
-}
-//call the function
-var sAmount = salesTaxAmount(originalPrice,salesPerc);
-
-//anon function that runs for item's price w/ tax
-var itemWithTax = function(op,sa,da){
-  var taxedItem = op+sa-da;
-  return taxedItem;
-}
-//call it
-var itemTaxTotal = itemWithTax(originalPrice, sAmount, dAmount);
-
-//anon function that runs for item's price w/o tax
-var itemWOTax = function(op,da){
-  var nonTaxedItem = op-da;
-  return nonTaxedItem;
-}
-//call it
-var itemNoTaxTotal = itemWOTax(originalPrice,dAmount);
-
-//print out result
-console.log("Your " + itemDescription + " was originally $" + originalPrice + ", but after a " + discountPercentage + "% discount, it is now $" + itemNoTaxTotal + " without tax, and $" + itemTaxTotal + " with tax.");
+//Output Area
+console.log("Your " + itemDescription + " was originally $" + originalPrice + ", but after a " + discountPercentage + "% discount, it is now $" + itemWithoutTax + " without tax, and $" + itemWithTax + " with tax.");
